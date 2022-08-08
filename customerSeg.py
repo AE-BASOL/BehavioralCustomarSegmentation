d5eea12a9ee25233908a64e3ef067cdc020e4e89
IDEATEKNOLOJI\ahmet.basol <ahmetefe.basol@ogr.iuc.edu.tr>
2022-08-08 15:06:16 +0300

    Davranışsal Müşteri Segmentasyonu (K-means)

---
+++ customerSeg.py
@@ -0,0 +1,162 @@
+# %% IMPORTING LIBRARIES
+
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt, numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import AgglomerativeClustering
+from matplotlib.colors import ListedColormap
+from sklearn import metrics
+import warnings
+import sys
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+np.random.seed(42)
+
+
+# %% LOADING DATA
+#Loading the dataset
+data = pd.read_csv("marketing_campaign.csv", sep="\t")
+print("Number of datapoints:", len(data))
+old_num = len(data)
+data.head()
+data = data.dropna() # removes rows with missing values
+print("The total number of data-points after removing the rows with missing values are:", len(data))
+print("Removed the rows with missing values are:", old_num-len(data))
+
+# %% DATE
+data["Dt_Customer"] = pd.to_datetime(data["Dt_Customer"]) # time'a convertliyor
+dates = []
+for i in data["Dt_Customer"]:
+    i = i.date()
+    dates.append(i)
+#Dates of the newest and oldest recorded customer
+print("The newest customer's enrolment date in therecords:",max(dates))
+print("The oldest customer's enrolment date in the records:",min(dates))
+
+#Created a feature "Customer_For"
+days = []
+d1 = max(dates) #taking it to be the newest customer
+for i in dates:
+    delta = d1 - i
+    print(delta, d1, i)
+    days.append(delta)
+data["Customer_For"] = days
+data["Customer_For"] = pd.to_numeric(data["Customer_For"], errors="coerce")
+
+# %%
+print("Total categories in the feature Marital_Status:\n", data["Marital_Status"].value_counts(), "\n")
+print("Total categories in the feature Education:\n", data["Education"].value_counts())
+
+# %%Feature Engineering
+#Age of customer today
+data["Age"] = 2021-data["Year_Birth"]
+
+#Total spendings on various items
+data["Spent"] = data["MntWines"]+ data["MntFruits"]+ data["MntMeatProducts"]+ data["MntFishProducts"]+ data["MntSweetProducts"]+ data["MntGoldProds"]
+
+#Deriving living situation by marital status"Alone"
+data["Living_With"]=data["Marital_Status"].replace({"Married":"Partner", "Together":"Partner", "Absurd":"Alone", "Widow":"Alone", "YOLO":"Alone", "Divorced":"Alone", "Single":"Alone",})
+
+#Feature indicating total children living in the household
+data["Children"]=data["Kidhome"]+data["Teenhome"]
+
+#Feature for total members in the householde
+data["Family_Size"] = data["Living_With"].replace({"Alone": 1, "Partner":2})+ data["Children"]
+
+#Feature pertaining parenthood
+data["Is_Parent"] = np.where(data.Children> 0, 1, 0)
+
+#Segmenting education levels in three groups
+data["Education"]=data["Education"].replace({"Basic":"Undergraduate","2n Cycle":"Undergraduate", "Graduation":"Graduate", "Master":"Postgraduate", "PhD":"Postgraduate"})
+
+#for clarity
+data=data.rename(columns={"MntWines": "Wines","MntFruits":"Fruits","MntMeatProducts":"Meat","MntFishProducts":"Fish","MntSweetProducts":"Sweets","MntGoldProds":"Gold"})
+
+#Dropping some of the redundant features
+to_drop = ["Marital_Status", "Dt_Customer", "Z_CostContact", "Z_Revenue", "Year_Birth", "ID"]
+data = data.drop(to_drop, axis=1)
+
+data.describe()
+
+# %% PLOT SELECTED FEATURES
+#Setting up colors prefrences
+sns.set(rc={"axes.facecolor":"#FFF9ED","figure.facecolor":"#FFF9ED"}) #küçük - büyük plotların arkaplanı
+pallet = ["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"]
+cmap = colors.ListedColormap(["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"])
+
+#Plotting following features
+To_Plot = [ "Income", "Recency", "Customer_For", "Age", "Spent", "Is_Parent"]
+print("Reletive Plot Of Some Selected Features: A Data Subset")
+plt.figure()
+sns.pairplot(data[To_Plot], hue= "Is_Parent",palette= (["#807d38","#F3AB60"]))
+
+#Taking hue
+plt.show()
+
+#Dropping the outliers by setting a cap on Age and income.
+data = data[(data["Age"]<90)]
+data = data[(data["Income"]<600000)]
+print("The total number of data-points after removing the outliers are:", len(data))
+
+#correlation matrix
+corrmat= data.corr()
+plt.figure(figsize=(20,20))
+sns.heatmap(corrmat,annot=True, cmap=cmap, center=0)
+
+# %% DATA PREPROCESSING
+#Get list of categorical variables
+s = (data.dtypes == 'object')
+print(s)
+object_cols = list(s[s].index)
+print("Categorical variables in the dataset:", object_cols)
+
+# Label Encoding the object dtypes.
+LE = LabelEncoder()
+for i in object_cols:
+    data[i] = data[[i]].apply(LE.fit_transform)
+print("All features are now numerical")
+
+#Creating a copy of data
+ds = data.copy()
+               # creating a subset of dataframe by dropping the features on deals accepted and promotions
+cols_del = ['AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'AcceptedCmp1','AcceptedCmp2', 'Complain', 'Response']
+ds = ds.drop(cols_del, axis=1)
+# %%Scaling
+scaler = StandardScaler()
+scaler.fit(ds)
+scaled_ds = pd.DataFrame(scaler.transform(ds),columns= ds.columns ) #Dataları standartize ediyoruz
+print("All features are now scaled")
+
+#Scaled data to be used for reducing the dimensionality
+print("Dataframe to be used for further modelling:")
+scaled_ds.head()
+
+# %% DIMENSIONALITY REDUCTION
+#Initiating PCA to reduce dimentions aka features to 3
+pca = PCA(n_components=3)
+pca.fit(scaled_ds)
+PCA_ds = pd.DataFrame(pca.transform(scaled_ds), columns=(["col1","col2", "col3"]))
+PCA_ds.describe().T
+
+#A 3D Projection Of Data In The Reduced Dimension
+x =PCA_ds["col1"]
+y =PCA_ds["col2"]
+z =PCA_ds["col3"]
+#To plot
+fig = plt.figure(figsize=(10,8))
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(x, y, z, c='red', marker="o")
+ax.set_title("A 3D Projection Of Data In The Reduced Dimension")
+plt.show()
+
